3807d25dfcd6bb80e0e0ddf958fe6f3f3b2e8de4
Rikuto <orikutobasuke@gmail.com>
2023-04-13 14:26:44 +0900

    nextframer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 export default function Index() {
   return (
-    <></>
+    <>
+      <div>
+        <ul>
+          <Link href={'./samples/01'}><li>Sample01</li></Link>
+          <Link href={'./samples/02'}><li>Sample02</li></Link>
+        </ul>
+      </div>
+    </>
   )
 }
