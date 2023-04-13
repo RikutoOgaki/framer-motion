a8429b48963b8161c334b1f543554d9fe10b243d
Rikuto <orikutobasuke@gmail.com>
2023-04-13 15:20:41 +0900

    nextframer

--- src/pages/samples/02/index.tsx
+++ src/pages/samples/02/index.tsx
@@ -1,9 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import style from './index.module.scss'
 
 export default function Sample02() {
+
+    const [state, setState] = useState({
+        x: 0,
+        y: 0
+    })
+
+    useEffect(() => {
+        const MousemoveEvent = (e) => {
+            setState({
+                ...state,
+                x: e.clientX,
+                y: e.clientY
+            })
+        }
+        window.addEventListener('mousemove', MousemoveEvent)
+
+        return () => {
+            window.removeEventListener('mousemove', MousemoveEvent)
+        }
+    }, [])
+
+
+
     return (
         <>
-
+            <div
+                className={style.pointer}
+                style={{ transform: `translate(${state.x}px , ${state.y}px)` }}
+            ></div>
+            <div
+                className={style.pointerMidll}
+                style={{ transform: `translate(${state.x}px , ${state.y}px)` }}
+            ></div>
+            <div
+                className={style.pointerSmall}
+                style={{ transform: `translate(${state.x}px , ${state.y}px)` }}
+            ></div>
         </>
     )
 }
