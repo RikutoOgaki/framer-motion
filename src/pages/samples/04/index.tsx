16e219b8cfc942be1b499ab9e68a398443e07c3b
Rikuto <orikutobasuke@gmail.com>
2023-04-14 10:34:36 +0900

    nextframer

--- src/pages/samples/04/index.tsx
+++ src/pages/samples/04/index.tsx
@@ -18,59 +18,61 @@ export default function Sample04() {
 
     return (
         <>
-            <motion.nav
-                initial={false}
-                animate={isOpen ? 'open' : 'closed'}
-                className={style.menu}
-            >
-                <motion.button
-                    whileTap={{ scale: .97 }}
-                    onClick={() => setIsOpen(!isOpen)}
-                >Menu
-                    <motion.div
-                        variants={{
-                            open: { rotate: 180 },
-                            closed: { rotate: 0 }
-                        }}
-                        transition={{ duration: .2 }}
-                        style={{ originY: .55 }}
-                    >
-                        <svg width={'15'} height={'15'} viewBox='0 0 20 20'>
-                            <path d='M0 7 L 20 7 L 10 16' />
-                        </svg>
-                    </motion.div>
-                </motion.button>
-                <motion.ul
-                    variants={{
-                        open: {
-                            clipPath: 'inset(0 0 0 0 round 10px)',
-                            transition: {
-                                type: 'spring',
-                                bounce: 0,
-                                duration: .7,
-                                delayChildren: .3,
-                                setggerChildren: .05
-                            }
-                        },
-                        closed: {
-                            clipPath: 'inset(10% 50% 90% 50% round 10px)',
-                            transition: {
-                                type: 'spring',
-                                bounce: 0,
-                                duration: .3
-                            }
-                        }
-                    }}
-                    style={{ pointerEvents: isOpen ? 'auto' : 'none' }}
+            <div className={style.wrap}>
+                <motion.nav
+                    initial={false}
+                    animate={isOpen ? 'open' : 'closed'}
+                    className={style.menu}
                 >
-                    <motion.li variants={itemVariants}>Item1</motion.li>
-                    <motion.li variants={itemVariants}>Item2</motion.li>
-                    <motion.li variants={itemVariants}>Item3</motion.li>
-                    <motion.li variants={itemVariants}>Item4</motion.li>
-                    <motion.li variants={itemVariants}>Item5</motion.li>
-                    <motion.li variants={itemVariants}>Item6</motion.li>
-                </motion.ul>
-            </motion.nav>
+                    <motion.button
+                        whileTap={{ scale: .97 }}
+                        onClick={() => setIsOpen(!isOpen)}
+                    >Menu
+                        <motion.div
+                            variants={{
+                                open: { rotate: 180 },
+                                closed: { rotate: 0 }
+                            }}
+                            transition={{ duration: .2 }}
+                            style={{ originY: .55 }}
+                        >
+                            <svg width={'15'} height={'15'} viewBox='0 0 20 20'>
+                                <path d='M0 7 L 20 7 L 10 16' />
+                            </svg>
+                        </motion.div>
+                    </motion.button>
+                    <motion.ul
+                        variants={{
+                            open: {
+                                clipPath: 'inset(0 0 0 0 round 10px)',
+                                transition: {
+                                    type: 'spring',
+                                    bounce: 0,
+                                    duration: .7,
+                                    delayChildren: .3,
+                                    setggerChildren: .05
+                                }
+                            },
+                            closed: {
+                                clipPath: 'inset(10% 50% 90% 50% round 10px)',
+                                transition: {
+                                    type: 'spring',
+                                    bounce: 0,
+                                    duration: .3
+                                }
+                            }
+                        }}
+                        style={{ pointerEvents: isOpen ? 'auto' : 'none' }}
+                    >
+                        <motion.li variants={itemVariants}>Item1</motion.li>
+                        <motion.li variants={itemVariants}>Item2</motion.li>
+                        <motion.li variants={itemVariants}>Item3</motion.li>
+                        <motion.li variants={itemVariants}>Item4</motion.li>
+                        <motion.li variants={itemVariants}>Item5</motion.li>
+                        <motion.li variants={itemVariants}>Item6</motion.li>
+                    </motion.ul>
+                </motion.nav>
+            </div>
         </>
     )
 }
