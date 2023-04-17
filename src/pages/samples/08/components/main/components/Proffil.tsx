0209590be3010ea4af8fe71508ff8b99b3bd673a
Rikuto <orikutobasuke@gmail.com>
2023-04-17 13:41:24 +0900

    next

--- src/pages/samples/08/components/main/components/Proffil.tsx
+++ src/pages/samples/08/components/main/components/Proffil.tsx
@@ -1,7 +1,16 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, Icon } from '@chakra-ui/react'
+import Link from 'next/link'
+import { SiGithub, SiInstagram, SiTwitter } from 'react-icons/si'
 
 
 export default function Proffil() {
+
+    const array = [
+        { icon: SiGithub, link: 'https://github.com/RikutoOgaki' },
+        { icon: SiInstagram, link: 'https://www.instagram.com/rikut0_0531/' },
+        { icon: SiTwitter, link: 'https://twitter.com/Rikuto_Ogaki' }
+    ]
+
     return (
         <>
             <Flex
@@ -32,6 +41,15 @@ export default function Proffil() {
                         fontSize={{ base: '3xl', md: '6xl' }}
                         color={'#fff'}
                         fontWeight={'bold'}>|</Text>
+                    <Flex w={'20%'} justifyContent={'space-around'}>
+                        {array.map((item, idx) =>
+                            <Link key={idx} href={item.link}>
+                                <Text color={'#fff'} fontSize={{ base: '3xl', md: '6xl' }} marginTop={'2rem'}>
+                                    <Icon as={item.icon} />
+                                </Text>
+                            </Link>
+                        )}
+                    </Flex>
                 </Flex>
             </Flex>
         </>
