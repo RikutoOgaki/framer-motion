8afdf1b3c7f092d2b2d6717a8b14ade786503a3e
Rikuto <orikutobasuke@gmail.com>
2023-04-17 13:31:58 +0900

    next

--- src/pages/samples/08/components/main/index.tsx
+++ src/pages/samples/08/components/main/index.tsx
@@ -1,11 +1,13 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import Prof from './components/Proffil'
+import Name from './components/Name'
 
 export default function Main() {
     return (
         <>
             <Box>
                 <Prof />
+                {/* <Name /> */}
             </Box >
         </>
     )
