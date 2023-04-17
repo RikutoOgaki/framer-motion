16eec348a604c87fe5fcf59fa8674b2d96af019d
Rikuto <orikutobasuke@gmail.com>
2023-04-17 10:46:08 +0900

    next

--- src/pages/samples/08/components/Header.tsx
+++ src/pages/samples/08/components/Header.tsx
@@ -5,29 +5,34 @@ export default function Header() {
     return (
         <>
             <header>
-                <Flex
-                    w={'100%'}
-                    h={'5vh'}
-                    justifyContent={'space-around'}
-                    alignItems={'center'}>
-                    <Link href={'/'}>
-                        <Text
-                            fontSize={{ base: '3xl', md: '5xl' }}
-                            fontWeight={'bold'}
-                            color={'#fff'}>TOP</Text></Link>
-                    <Link href={'/'}>
-                        <Text
-                            fontSize={{ base: '3xl', md: '5xl' }}
-                            fontWeight={'bold'}
-                            color={'#fff'}
-                        >Skills</Text></Link>
-                    <Link href={'/'}>
-                        <Text
-                            fontSize={{ base: '3xl', md: '5xl' }}
-                            fontWeight={'bold'}
-                            color={'#fff'}
-                        >Works</Text></Link>
-                </Flex>
+                <Box position={'relative'}>
+                    <Flex
+                        w={'100%'}
+                        h={'5vh'}
+                        justifyContent={'space-around'}
+                        alignItems={'center'}
+                        position={'absolute'}
+                        top={0}
+                    >
+                        <Link href={'/'}>
+                            <Text
+                                fontSize={{ base: '3xl', md: '5xl' }}
+                                fontWeight={'bold'}
+                                color={'#fff'}>TOP</Text></Link>
+                        <Link href={'/'}>
+                            <Text
+                                fontSize={{ base: '3xl', md: '5xl' }}
+                                fontWeight={'bold'}
+                                color={'#fff'}
+                            >Skills</Text></Link>
+                        <Link href={'/'}>
+                            <Text
+                                fontSize={{ base: '3xl', md: '5xl' }}
+                                fontWeight={'bold'}
+                                color={'#fff'}
+                            >Works</Text></Link>
+                    </Flex>
+                </Box>
             </header>
         </>
     )
---
+++ src/pages/samples/08/components/main/components/Proffil.tsx
@@ -0,0 +1,12 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+
+
+export default function Proffil() {
+    return (
+        <>
+            <Box>
+
+            </Box>
+        </>
+    )
+}
