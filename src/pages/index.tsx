238f7dcc352ca7f53a9bff3e3af55a29074d5687
Rikuto <orikutobasuke@gmail.com>
2023-04-17 10:00:00 +0900

    nextframer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,10 @@ export default function Index() {
           <Link href={'./samples/02'}><li>Sample02:マウスストーカー</li></Link>
           <Link href={'./samples/03'}><li>Sample03:スクロールに合わせてバーが出てくる</li></Link>
           <Link href={'./samples/04'}><li>Sample04:メニュー表２</li></Link>
+          <Link href={'./samples/05'}><li>Sample05:</li></Link>
+          <Link href={'./samples/06'}><li>Sample06:HoverButton</li></Link>
+          <Link href={'./samples/07'}><li>Sample07:DragBox</li></Link>
+          <Link href={'./samples/08'}><li>Sample08:</li></Link>
         </ul>
       </div>
     </>
