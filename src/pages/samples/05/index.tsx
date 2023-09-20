82ee79ab0ce74bcd05a9ae017a20e6703ed0397e
Rikuto <orikutobasuke@gmail.com>
2023-09-20 16:15:58 +0900

    ファイル整理

--- src/pages/samples/05/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Sample05() {
-    return (
-        <></>
-    )
-}
--- src/pages/samples/10/index.tsx
+++
@@ -1,9 +0,0 @@
-import { motion } from 'framer-motion'
-
-
-export default function Sample10() {
-    return (
-        <>
-        </>
-    )
-}
