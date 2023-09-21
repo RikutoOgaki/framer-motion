a9f865f8276d178245f3d5dfcc71bb390711db5e
Rikuto <orikutobasuke@gmail.com>
2023-09-21 19:09:19 +0900

    framer-motionの勉強中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <>
       <div>
         <ul>
-          <Link href={'./samples/01'}><li>Sample01</li></Link>
+          <Link href={'./sample/01'}><li>Sample01</li></Link>
           <Link href={'./samples/02'}><li>Sample02</li></Link>
         </ul>
       </div>
--- src/pages/sample/01/index.tsx
+++ src/pages/sample/01/index.tsx
@@ -1,5 +1,20 @@
+import { motion } from 'framer-motion'
+import { useState, useEffect } from 'react'
+import style from '@/styles/sample/01/index.module.scss'
+
 export default function Sample01() {
+
+    // 一つのBoxを自由に場所配置できるような例の実装をする
+
     return (
-        <></>
+        <>
+            <div className={style.wrap}>
+                <motion.div
+                    className={style.box}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                />
+            </div>
+        </>
     )
 }
