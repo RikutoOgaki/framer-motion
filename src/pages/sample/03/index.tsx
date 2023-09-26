cc3ae9aeb6b499c8b918652f43522c1c75642ee7
Rikuto <orikutobasuke@gmail.com>
2023-09-27 00:36:33 +0900

    framer-motion,sample03完成

--- src/pages/sample/03/index.tsx
+++ src/pages/sample/03/index.tsx
@@ -10,14 +10,16 @@ export default function Sample03() {
                 <motion.div
                     className={style.box}
                     animate={{
-                        scale: [1, 1, 2, 2, 1],
+                        scale: [1, 2, 2, 1, 1],
                         rotate: [0, 0, 180, 180, 0],
                         borderRadius: ['0%', '0%', '50%', '50%', '0%']
                     }}
                     transition={{
                         duration: 2,
-                        ease: easeInOut,
-
+                        ease: 'easeInOut',
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        repeat: Infinity,
+                        repeatDelay: 1
                     }}
                 />
             </div>
