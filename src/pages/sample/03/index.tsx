c441bcf69280b8e83355ac83f870db385b4b2da0
Rikuto <orikutobasuke@gmail.com>
2023-09-26 22:06:55 +0900

    framer-motionの途中

--- src/pages/sample/03/index.tsx
+++ src/pages/sample/03/index.tsx
@@ -1,16 +1,25 @@
-import { motion } from 'framer-motion'
+import { easeInOut, motion } from 'framer-motion'
 import { useState, useEffect } from 'react'
 import style from '@/styles/sample/03/index.module.scss'
 
 export default function Sample03() {
 
-    // framer-motionを使って実際に動くようなものを作る
-    // 内容はスケール系の内容を作る
-
     return (
         <>
             <div className={style.wrap}>
+                <motion.div
+                    className={style.box}
+                    animate={{
+                        scale: [1, 1, 2, 2, 1],
+                        rotate: [0, 0, 180, 180, 0],
+                        borderRadius: ['0%', '0%', '50%', '50%', '0%']
+                    }}
+                    transition={{
+                        duration: 2,
+                        ease: easeInOut,
 
+                    }}
+                />
             </div>
         </>
     )
